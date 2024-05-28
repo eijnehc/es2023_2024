3136f9c03a0aee7efb350a85b43305e40c2dafb1
Chen Jie <61531625+eijnehc@users.noreply.github.com>
2024-05-29 00:29:29 +0800

    lesson_4: iterator helpers

---
+++ iterator.js
@@ -0,0 +1,20 @@
+const arr = [1, 2, 3, 4, 5, 6, 8, 9, 10];
+
+arr.map((x) => x ** 2); // [1,  4,  9,  16, 25, 36, 64, 81, 100]
+
+function* map(iterable, mapperFn) {
+  for (const item of iterable) {
+    yield mapperFn(item);
+  }
+}
+
+const iter = map(arr, (x) => x ** 2);
+
+console.log([...iter]); // [1,  4,  9,  16, 25, 36, 64, 81, 100]
+
+//////////////////////
+// alternative
+// lazily calculate
+const iterHelper = arr.values().map((x) => x ** 2);
+
+[...iterHelper];
